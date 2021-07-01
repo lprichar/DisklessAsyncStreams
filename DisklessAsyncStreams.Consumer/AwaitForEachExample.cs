d2242318b0a346f9d48595e91f3e50a7d90c9446
Lee Richardson <github@leerichardson.com>
2021-06-30 21:39:03 -0400

    Code sample from blog post

---
+++ DisklessAsyncStreams.Consumer/AwaitForEachExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DisklessAsyncStreams.Consumer
+{
+    class AwaitForEachExample
+    {
+        public async Task IterateNumbersTo10()
+        {
+            IAsyncEnumerable<int> numbers = Producer.GetNumbersAsync();
+            await foreach (int number in numbers)
+            {
+                if (number > 10)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ DisklessAsyncStreams.Consumer/Producer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DisklessAsyncStreams.Consumer
+{
+    internal class Producer
+    {
+        public static async IAsyncEnumerable<int> GetNumbersAsync()
+        {
+            int i = 0;
+            while (true)
+            {
+                await Task.Delay(1);
+                yield return i;
+                i++;
+            }
+        }
+    }
+}
